f47c1bfef065ef8abafa00a0a8e9d1bda4c0d0aa
lengsp <lengsoonpak@gmail.com>
2021-12-31 15:55:32 +0800

    Add files via upload

--- node_helper.js
+++ node_helper.js
@@ -1,5 +1,6 @@
 let NodeHelper = require('node_helper');
 let fetch = require('node-fetch');
+let cheerio = require('cheerio');
 
 module.exports = NodeHelper.create({
   start: function () {
@@ -9,16 +10,41 @@ module.exports = NodeHelper.create({
   getJson: function (url) {
     let self = this;
 
-    fetch(url).then(response => response.json()).then(json => {
-      // Send the json data back with the url to distinguish it on the receiving part
-      self.sendSocketNotification("MMM-JsonTable_JSON_RESULT", {url: url, data: json});
-    });
-  },
+ 
+		fetch(url).then(response => response.text()).then(html => {
+			// Send the json data back with the url to distinguish it on the receiving part
+			var $ = cheerio.load(html);		
+			var contents = [];
+			
+			 $('table.table.table-borderless.fuel_comparison_table.mb-0 tr td').each(function(i, element)
+				{
+					contents[i] = $(this).text().replace(/\r|\n/g,",");
+				}     
+			);
+			
+			
+			var fuel = [];
+			for (let i = 0; i < 5; i++) {
+			fuel[i] = { "type": contents[i+(i*5)], "esso": contents[i+(i*5)+1], "shell":contents[i+(i*5)+2], "spc": contents[i+(i*5)+3], "caltex":contents[i+(i*5)+4], "sinopec":contents[i+(i*5)+5]}; 
+			}
+			
+			
+			let result = JSON.stringify(fuel);
+			
 
+			self.sendSocketNotification("MMM-JsonTable_JSON_RESULT", {url: url, data: fuel});
+		});
+  
+  },  
+  
+  
   //Subclass socketNotificationReceived received.
-  socketNotificationReceived: function (notification, url) {
-    if (notification === "MMM-JsonTable_GET_JSON") {
-      this.getJson(url);
-    }
-  }
+	socketNotificationReceived: function (notification, url) {
+		if (notification === "MMM-JsonTable_GET_JSON") {
+			this.getJson(url);
+		}
+
+	}
+  
+  
 });
